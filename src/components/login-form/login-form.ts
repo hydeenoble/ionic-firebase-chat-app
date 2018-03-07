985e42c57f1c2585a88833b8389f07eb1583a351
Emehinola Idowu <hydeenoble39@gmail.com>
2018-03-07 03:16:22 +0100

    Authenticate Users with Firebase and AngularFire2

--- src/components/login-form/login-form.ts
+++ src/components/login-form/login-form.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from "ionic-angular";
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { Account } from "../../models/account/account";
 
 /**
  * Generated class for the LoginFormComponent component.
@@ -14,10 +17,23 @@ import { NavController } from "ionic-angular";
 export class LoginFormComponent {
 
 
-  constructor(private navCtrl: NavController ) {
+  account = {} as Account;
+
+  constructor(private afAuth: AngularFireAuth, private navCtrl: NavController ) {
     console.log('Hello LoginFormComponent Component');
   }
 
+  async login(){
+
+    try{
+      const result = await this.afAuth.auth
+        .signInWithEmailAndPassword(this.account.email, this.account.password);
+      console.log(result);
+    }catch (e){
+      console.log(e);
+    }
+
+  }
   naviagteToPage(pageName: string){
     pageName === 'TabsPage' ? this.navCtrl.setRoot(pageName) : this.navCtrl.push(pageName);
   }
--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 import { ComponentsModule } from "../../components/components.module";
-
+import { AngularFireAuthModule } from 'angularfire2/auth'
 @NgModule({
   declarations: [
     LoginPage,
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
-    ComponentsModule
+    ComponentsModule,
+    AngularFireAuthModule
   ],
 })
 export class LoginPageModule {}
