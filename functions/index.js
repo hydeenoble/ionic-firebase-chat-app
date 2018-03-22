f2f74e92846693daebb1fcee823163e8af358d85
Emehinola Idowu <hydeenoble39@gmail.com>
2018-03-22 20:24:15 +0100

    Generate last mes messages with cloud functions

--- functions/index.js
+++ functions/index.js
@@ -22,6 +22,19 @@ exports.addUserMessages = functions.database.ref(`/messages/{messageId}`)
       .child(messageKey).set(1);
 });
 
+exports.generateLastMessage = functions.database.ref(`/messages/{messageId}`)
+  .onWrite(event => {
+    const messageKey = event.data.key;
+    const messageValue = event.data.val();
+
+    admin.database().ref(`/last-messages/${messageValue.userFromId}/${messageValue.userToId}`)
+      .child('key').set(messageKey);
+
+    admin.database().ref(`/last-messages/${messageValue.userToId}/${messageValue.userFromId}`)
+      .child('key').set(messageKey);
+
+  });
+
 
 
 
